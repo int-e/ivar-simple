e35132f322927755f351b01a44ca5aef0d6cc7fe
Bertram Felgenhauer <int-e@gmx.de>
2020-10-06 00:35:11 +0200

    save one `MVar` per `IVar`
    The original reason for having both a lock field and a field holding
    the value of the `IVar` was that there was no way to read an `MVar`
    without temporarily emptying it; `readMVar` used to do `takeMVar`
    followed by `putMVar`. This changed a while ago, and `readMVar` is
    now atomic. This allows the `IVar` code to be simplified.

--- src/Data/IVar/Simple.hs
+++ src/Data/IVar/Simple.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE DeriveDataTypeable, CPP #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 -- |
 -- Module      : Data.IVar.Simple
--- Copyright   : (c) 2008-2015 Bertram Felgenhauer
+-- Copyright   : (c) 2008-2020 Bertram Felgenhauer
 -- License     : MIT
 --
 -- Maintainer  : Bertram Felgenhauer <int-e@gmx.de>
@@ -35,42 +35,41 @@ import System.IO.Unsafe
 import Prelude hiding (read)
 
 -- | A write-once (/immutable/) Variable
-data IVar a = IVar (MVar ()) (MVar a) a
+data IVar a = IVar (MVar a) a
 
 instance Eq (IVar a) where
-    IVar lock1 _ _ == IVar lock2 _ _ = lock1 == lock2
+    IVar trans1 _ == IVar trans2 _ = trans1 == trans2
     -- note: it would be possible to generalize the type to
     -- eqIVar :: IVar a -> IVar b -> Bool  (but would that be useful?)
 
 -- | Create a new, empty 'IVar'.
 new :: IO (IVar a)
 new = do
-    lock <- newMVar ()
     trans <- newEmptyMVar
     let {-# NOINLINE value #-}
-        value = unsafePerformIO $ takeMVar trans
-    return (IVar lock trans value)
+        value = unsafePerformIO $ readMVar trans
+    return (IVar trans value)
 
 -- | Create a new filled 'IVar'.
 --
 -- This is slightly cheaper than creating a new 'IVar' and then writing to it.
 newFull :: a -> IO (IVar a)
 newFull value = do
-    lock <- newEmptyMVar
-    return (IVar lock (error "unused MVar") value)
+    trans <- newMVar (error "unused value")
+    return (IVar trans value)
 
 -- | Returns the value of an 'IVar'.
 --
 -- The evaluation will block until a value is written to the 'IVar' if it
 -- has no value yet.
 read :: IVar a -> a
-read (IVar _ _ value) = value
+read (IVar _ value) = value
 
 -- | Try to read an 'IVar'. Returns 'Nothing' if it has no value yet.
 tryRead :: IVar a -> IO (Maybe a)
-tryRead (IVar lock _ value) = do
-    empty <- isEmptyMVar lock
-    if empty then return (Just value) else return Nothing
+tryRead (IVar trans value) = do
+    empty <- isEmptyMVar trans
+    if not empty then return (Just value) else return Nothing
 
 -- | Writes a value to an 'IVar'. Raises a 'BlockedIndefinitelyOnIVar'
 -- exception if the variable already has a value.
@@ -84,15 +83,7 @@ write ivar value = do
 -- | Writes a value to an 'IVar'. Returns 'True' if successful, and
 -- 'False' otherwise.
 tryWrite :: IVar a -> a -> IO Bool
-tryWrite (IVar lock trans _) value = block $ do
-    a <- tryTakeMVar lock
-    case a of
-        Just _  -> putMVar trans value >> return True
-        Nothing -> return False
-#if __GLASGOW_HASKELL__ >= 708
-  where
-    block = mask_
-#endif
+tryWrite (IVar trans _) value = tryPutMVar trans value
 
 -- | The thread has attempted to write to a full 'IVar'.
 data BlockedIndefinitelyOnIVar = BlockedIndefinitelyOnIVar
